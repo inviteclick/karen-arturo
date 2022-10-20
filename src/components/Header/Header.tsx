9712f62f07a16ca8178fcab286bbc98084be11b6
Javier Saviñon <dpllgr@hotmail.com>
2022-10-19 22:11:08 -0500

    fonts implemented

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+export const Header = () => {
+  return (
+    <div className={styles.head}>
+      <div className={styles.subtitle1}>
+        HABIENDO ESPERADO CON PACIENCIA ALCANZARON LA PROMESA DE DIOS
+      </div>
+      <h1 className={styles.title}>Arturo y Karen</h1>
+    </div>
+  );
+};
