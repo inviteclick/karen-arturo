d3c18aebe5163a79c5bf858380fe76a33a3af93b
Javier Saviñon <dpllgr@hotmail.com>
2022-10-24 15:05:03 -0500

    descargar invitacion DONE

--- src/components/MesadeRegalo/MesadeRegalo.tsx
+++ src/components/MesadeRegalo/MesadeRegalo.tsx
@@ -5,7 +5,16 @@ export const MesadeRegalo = () => {
   return (
     <div className={styles.main}>
       <div className={styles.title}>Mesa de Regalos </div>
-      <img src={liverpool} alt="liverpool" />
+      <a
+        href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51005862"
+        target="_blank"
+      >
+        <img src={liverpool} alt="liverpool" />
+      </a>
+      <span>
+        Num: 51005862 <br />
+        Ana Karen Zaleta y Arturo Gonzalez{" "}
+      </span>
     </div>
   );
 };
